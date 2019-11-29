d461413263127240ec1cecc0f53ca751eb31bcf3
Eric Smith <ericsmith.100@gmail.com>
2019-11-29 15:55:38 -0500

    remove tvjs

--- example/src/pages/index.js
+++ example/src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
-import TV from 'gatsby-theme-minimal/src/components/TV.js'
+import Index from 'gatsby-theme-minimal/src/pages/index.js'
 
 export default () => (
     <div>
-        <TV />
+        <Index />
     </div>
 )
